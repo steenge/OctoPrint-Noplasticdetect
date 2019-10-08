ef3bfac7e89158ac36ab831adad20028a83333f4
Steen Garbers Enevoldsen <steen@garbers.dk>
2019-10-08 15:00:55 +0200

    initial comit

---
+++ octoprint_noplasticdetect/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+        def on_after_startup(self):
+                self._logger.info("Hello World!")
+
+__plugin_name__ = "NoPlasticDetect"                
+__plugin_implementation__ = HelloWorldPlugin()
+                            
